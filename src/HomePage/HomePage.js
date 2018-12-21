79e83592f7d2ee2ca33b7d41860fbdba3d51cc1f
Rostyslav Mazepa <rostyslav.mazepa@gmail.com>
2018-12-21 14:44:16 +0200

    corrected homepage

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import { HomeContent } from "./HomeContent";
 import HeaderMenu from "./HeaderMenu";
 import SiderMenu from "./SiderMenu";
 
@@ -17,12 +18,8 @@ class HomePage extends React.Component {
     this.props.dispatch(userActions.getAll());
   }
 
-  handleDeleteUser(id) {
-    return e => this.props.dispatch(userActions.delete(id));
-  }
-
   onCollapse = collapsed => {
-    console.log(collapsed);
+    //console.log(collapsed);
     this.setState({ collapsed });
   };
 
@@ -47,40 +44,13 @@ class HomePage extends React.Component {
             <HeaderMenu />
           </Header>
           <Content style={{ margin: "0 16px" }}>
-            <div className="col-md-6 col-md-offset-3">
-              <h1>Hi {user.firstName}!</h1>
-              <p>You're logged in with React!!</p>
-              <h3>All registered users:</h3>
-              {users.loading && <em>Loading users...</em>}
-              {users.error && (
-                <span className="text-danger">ERROR: {users.error}</span>
-              )}
-              {users.items && (
-                <ul>
-                  {users.items.map((user, index) => (
-                    <li key={user.id}>
-                      {user.firstName + " " + user.lastName}
-                      {user.deleting ? (
-                        <em> - Deleting...</em>
-                      ) : user.deleteError ? (
-                        <span className="text-danger">
-                          {" "}
-                          - ERROR: {user.deleteError}
-                        </span>
-                      ) : (
-                        <span>
-                          {" "}
-                          -{" "}
-                          <a onClick={this.handleDeleteUser(user.id)}>Delete</a>
-                        </span>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              )}
-              <p>
-                <Link to="/login">Logout</Link>
-              </p>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+              Bill is a cat.
+              <HomeContent dataUsers={users} dataUser={user} />
             </div>
           </Content>
           <Footer style={{ textAlign: "center" }}>
